ebeef47c8e939d2536c9d73aac77313d34d855b6
ferbaqfer <ferbaqfer@alum.us.es>
2023-03-03 11:50:04 +0100

    Creación entidad Student
    Refs #23

--- src/main/java/acme/roles/Student.java
+++ src/main/java/acme/roles/Student.java
@@ -1,6 +1,41 @@
+
 package acme.roles;
 
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Student {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			statement;
+
+	@NotBlank
+	@Length(max = 100)
+	protected List<String>		strongFeatures;
+
+	@NotBlank
+	@Length(max = 100)
+	protected List<String>		weakFeatures;
+
+	@URL
+	protected String			link;
 
 }
